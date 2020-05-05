e64d104616b3781d257c498776774bfda7094d30
Karama Aissaoui <aissaoui.karama@gmail.com>
2020-05-05 16:17:38 +0100

    Adding calendar

---
+++ src/components/calendar/calendar-component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default class Schedule extends Component {
+  // declare any necessary functions such as handleDateClick, etc.
+
+  render() {
+    return (
+        <p>Full calendar</p>
+    )
+  }
+}
